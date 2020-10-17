48d08000b5929df158c9c730f1059868ca2865ff
Ваше Имя <azizzharr>
2020-10-17 20:57:24 +0600

    dwq

---
+++ 12-x.js
@@ -0,0 +1,20 @@
+const readLine = require('readline').createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+readLine.on('line',word1=>{
+   const a = +'';
+for (let i =2;i>a ;i++){
+    if(a%i===0){
+        console.log('жонокой сан эмес')
+    }else {
+        console.log('жонокой сан')
+
+    }
+    break;
+
+}
+
+        readLine.close()
+})
